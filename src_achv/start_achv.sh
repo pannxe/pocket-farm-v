2f6dcc5b2dc769d5ecfb3a046cca0a06276ee4a6
Krit Patrix <kzt.patrix@gmail.com>
2021-11-17 16:23:32 +0700

    :coffin: remove old codebase

--- src_achv/start_achv.sh
+++
@@ -1,5 +0,0 @@
-  gnome-terminal -x bash -c "./src/resetf && exit; exec bash"\
-& gnome-terminal -x bash -c "./src/fman; exec bash"\
-& gnome-terminal -x bash -c "python3 ./src/sever_comm.py && exit; exec bash"\
-& gnome-terminal -x bash -c "python3 ./src/sensor_reader.py && exit; exec bash"\
-& gnome-terminal -x bash -c "python3 ./src/hardware_man.py && exit; exec bash"&
